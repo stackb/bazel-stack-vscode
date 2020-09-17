b9cd7a516617bdcafb3591a0d7daf01167143ae6
Paul Johnston <paul.johnston02@sap.com>
2020-09-17 16:04:00 -0600

    Add help commands to package.json

--- src/bzl/view/license.ts
+++ src/bzl/view/license.ts
@@ -118,6 +118,7 @@ export class LicenseItem extends vscode.TreeItem {
     ) {
         super(label);
         if (iconUrl) {
+            // @ts-ignore
             this.iconPath = vscode.Uri.parse(iconUrl);
         }
     }
