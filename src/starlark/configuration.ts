229d946397338d06eb4d38929eb483f3d05fd13b
Paul Code Johnston <pcj@stack.build>
2020-10-25 22:47:59 -0600

    Fix lsp config sections

--- src/starlark/configuration.ts
+++ src/starlark/configuration.ts
@@ -21,7 +21,7 @@ export type ServerConfiguration = {
 export async function createStarlarkLSPConfiguration(ctx: vscode.ExtensionContext, config: vscode.WorkspaceConfiguration): Promise<StarlarkLSPConfiguration> {
     const server = {
         owner: config.get<string>(ConfigSection.LspServerGithubOwner, 'stackb'),
-        repo: config.get<string>(ConfigSection.LspServerGithubOwner, 'bzl'),
+        repo: config.get<string>(ConfigSection.LspServerGithubRepo, 'bzl'),
         releaseTag: config.get<string>(ConfigSection.LspServerGithubRelease, '0.9.4'),
         executable: config.get<string>(ConfigSection.LspServerExecutable, ''),
         command: config.get<string[]>(ConfigSection.LspServerCommand, ['lsp', 'serve']),
