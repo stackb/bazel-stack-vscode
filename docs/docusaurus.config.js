8f5f11f5e747e31ac65d66323c30a55a24ad80fa
Paul Johnston <paul.johnston@robinhood.com>
2021-12-03 00:48:54 -0700

    Final changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,31 +1,60 @@
 module.exports = {
   title: 'bazel-stack-vscode',
   tagline: 'Bazel developer productivity suite',
-  url: 'https://bzl.io',
-  baseUrl: '/bzl/',
+  url: 'https://docs.stack.build',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'https://stackb.github.io/bazel-stack-vscode/logos/stack-build.png',
   organizationName: 'stackb', // Usually your GitHub org/user name.
-  projectName: 'bazel-stack-vscode', // Usually your repo name.
+  projectName: 'stack.build', // Usually your repo name.
   themeConfig: {
-    defaultMode: 'dark',
+    defaultMode: 'light',
     navbar: {
-      title: 'docs.bzl.io',
+      title: 'Home',
       logo: {
-        alt: 'docs.bzl.io',
+        alt: 'Documentation',
         src: 'https://stackb.github.io/bazel-stack-vscode/logos/stack-build.png',
       },
       items: [
         {
-          to: 'docs/',
+          to: 'docs/vscode/installation',
           activeBasePath: 'docs',
-          label: 'Docs',
+          label: 'Bazel-Stack-VSCode',
+          position: 'left',
+        },
+        {
+          to: 'docs/cli/installation',
+          activeBasePath: 'docs',
+          label: 'Bzl CLI',
+          position: 'left',
+        },
+        {
+          to: 'docs/remote-cache/overview',
+          activeBasePath: 'docs',
+          label: 'Bzl Remote Cache',
+          position: 'left',
+        },
+        {
+          to: 'docs/ui/overview',
+          activeBasePath: 'docs',
+          label: 'Bezel UI',
+          position: 'left',
+        },
+        {
+          to: 'docs/invocations/overview',
+          activeBasePath: 'docs',
+          label: 'Build Results UI',
           position: 'left',
         },
         {
           type: 'docsVersionDropdown',
           position: 'right',
           dropdownActiveClassDisabled: true,
+        },
+        {
+          href: 'https://bzl.io',
+          label: 'BZL.IO',
+          position: 'right',
         },
         {
           href: 'https://github.com/stackb/bazel-stack-vscode',
--- docs/src/pages/index.md
+++ docs/src/pages/index.md
@@ -3,30 +3,18 @@ title: bazel-stack-vscode
 hide_table_of_contents: true
 ---
 
-# bazel-stack-vscode
+![logo](https://user-images.githubusercontent.com/50580/78734724-3c7fe200-7906-11ea-89b2-ddedb444f3b4.png)
 
-<table><tbody><tr>
-<td className="center max-width-120"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Bazel_logo.svg/240px-Bazel_logo.svg.png" /></td>
-<td className="center max-width-120"><img src="https://user-images.githubusercontent.com/50580/78734740-486ba400-7906-11ea-89fa-f207544de185.png" /></td>
-<td className="center max-width-120"><img src="https://user-images.githubusercontent.com/29654835/27530004-e789a11e-5a13-11e7-8a34-870da7e678ac.PNG"  /></td>
-</tr><tr>
-<td className="center">Bazel</td>
-<td className="center">Stack</td>
-<td class="center">VSCode</td>
-</tr></tbody></table>
+# Bazel Developer Productivity Toolsuite
 
-## Documentation
+## Services
 
-<https://stackb.github.io/bazel-stack-vscode/>
+- [bzl.io](https://bzl.io)
 
-## Marketplace
+## Tools
 
-<https://marketplace.visualstudio.com/items?itemName=StackBuild.bazel-stack-vscode>
-
-VSCode editor support for the starlark language and the bazel dialect thereof.
-
-The following operating systems are supported:
-
-- linux
-- mac
-- windows
+- [VSCode IDE Support](docs/vscode/installation)
+- [Bzl CLI](docs/cli/installation)
+- [Bzl Remote Cache](docs/remote-cache/overview)
+- [Bezel Build UI](docs/ui/overview)
+- [Build Results UI](docs/invocations/overview)
